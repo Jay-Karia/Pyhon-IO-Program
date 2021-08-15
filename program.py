d201fe211e3b5bfd187c52efc8307bf276c4a739
Jay <jay.sanjay.karia@gmail.com>
2021-08-15 13:28:28 +0530

    Working on Writing to a File

--- program.py
+++ program.py
@@ -1,6 +1,8 @@
 import os
+
 print("Welcome to Python IO Program Developed -- By Jay")
 sel = input("\n1) Custom Directory\n2) Default Directory\n")
+
 
 # Methods
 def list(directory):
@@ -9,27 +11,44 @@ def list(directory):
     for items in li:
         print(items)
 
+
 def read(directory, filename):
-    with open(directory + filename, "w") as read:
+    with open(directory + filename) as reading:
         print(f"\nReading from {filename}...\n")
-        for data in read:
+        for data in reading:
             print(data)
+        print("\nReading Finished from a File!")
+
+def writeToAFile(directory, filename):
+    with open((directory + filename), "w") as writing:
+        print("Enter \'X\' to stop writing...")
+        w = ""
+        while w != "X":
+            w = input()
+            wr = w.replace("W", "")
+            writing.write(wr)
+        print("\nWriting Finished To a File!")
+
 
 if sel == "1":
     print("\nSelected 1) Custom Directory\n")
     str = "Enter you Directory: "
     dir = input(str)
-    cDir = dir+"\\"
+    cDir = dir + "\\"
     print(f"Directory is set to: " + cDir)
 else:
     print("Selected 2) Default Directory\n1")
     cDir = os.getcwd() + "\\"
     print(f"Directory is set to: " + cDir)
-    
-opt = input("\nEnter what do you want do:\n1) List the items in the Directory\n2) Read a file\n3) Write into a file\n4) Append to a file\n5) Create a new file\n6) Delete a File\n")
+
+opt = input(
+    "\nEnter what do you want do:\n1) List the items in the Directory\n2) Read a file\n3) Write into a file\n4) Append to a file\n5) Create a new file\n6) Delete a File\n")
 
 if opt == "1":
     list(cDir)
 elif opt == "2":
     name = input("Enter file name: ")
     read(cDir, name)
+elif opt == "3":
+    name = input("Enter file name: ")
+    writeToAFile(cDir, name)
