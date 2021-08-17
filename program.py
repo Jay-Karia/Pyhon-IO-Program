5fd9d790fa9c522433e7a4f9b9fb7558793d034c
Jay <jay.sanjay.karia@gmail.com>
2021-08-17 18:59:14 +0530

    100% Done! Maybe.....

--- program.py
+++ program.py
@@ -11,7 +11,6 @@ def list(directory):
     for items in li:
         print(items)
 
-
 def read(directory, filename):
     with open(directory + filename) as reading:
         print(f"\nReading from {filename}...\n")
@@ -42,8 +41,13 @@ def appendToAFile(directory, filename):
         print("Appending Finished To a File!")
 
 def newDir(directory):
-    dir = input(f"Enter the name of the folder at {directory}")
+    dir = input(f"Enter the name of the folder at:\n {directory}")
     os.mkdir(directory + dir)
+    print("Directory Created Successfully!")
+
+def newDirs(directory):
+    dir = input(f"Enter the name of the folder at:\n {directory}")
+    os.makedirs(directory + dir)
     print("Directory Created Successfully!")
 
 if sel == "1":
@@ -58,18 +62,23 @@ else:
     print(f"Directory is set to: " + cDir)
 
 opt = input(
-    "\nEnter what do you want do:\n1) List the items in the Directory\n2) Read a file\n3) Write into a file\n4) Append to a file\n5) Create a new directory\n6) Create new directories\nlol")
+    "\nEnter what do you want do:\n1) List the items in the Directory\n2) Read a file\n3) Write into a file\n4) Append to a file\n5) Create a new directory\n6) Create new directories\n")
 
-if opt == "1":
-    list(cDir)
-elif opt == "2":
-    name = input("Enter file name: ")
-    read(cDir, name)
-elif opt == "3":
-    name = input("Enter file name: ")
-    writeToAFile(cDir, name)
-elif opt == "4":
-    name = input("Enter file name: ")
-    appendToAFile(cDir, name)
-elif opt == "5":
-    newDir(cDir)
+try:
+    if opt == "1":
+        list(cDir)
+    elif opt == "2":
+        name = input("Enter file name: ")
+        read(cDir, name)
+    elif opt == "3":
+        name = input("Enter file name: ")
+        writeToAFile(cDir, name)
+    elif opt == "4":
+        name = input("Enter file name: ")
+        appendToAFile(cDir, name)
+    elif opt == "5":
+        newDir(cDir)
+    elif opt == "6":
+        newDirs(cDir)
+except:
+    print(f"An Error Occurred! Sorry")
